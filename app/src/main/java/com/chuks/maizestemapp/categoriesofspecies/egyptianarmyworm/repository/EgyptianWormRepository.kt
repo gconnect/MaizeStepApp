86e8056ebe818c967229be651ad5ee58753d5b42
gconnect <agatevureglory@gmail.com>
2020-03-08 09:29:22 +0100

    This is the interface representation of the egyptian armyworm

---
+++ app/src/main/java/com/chuks/maizestemapp/categoriesofspecies/egyptianarmyworm/repository/EgyptianWormRepository.kt
@@ -0,0 +1,19 @@
+package com.chuks.maizestemapp.categoriesofspecies.egyptianarmyworm.repository
+
+import androidx.lifecycle.LiveData
+import com.chuks.maizestemapp.common.data.Insect
+
+/**
+ * This is EgyptianWormRepository Interface
+ * */
+interface EgyptianWormRepository {
+    /**
+     * This [getEgyptianArmyWorm] and return a liveData of Insects
+     * */
+    fun getEgyptianArmyWorm() : LiveData<List<Insect>>
+
+    /**
+     * This [requestAfricanArmyWorm] and insert into the database
+     * */
+    suspend fun requestEgyptianArmyWorm()
+}
