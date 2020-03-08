ce9e6b8ffb81307f5d26f15f8c28735e8f5b7d95
gconnect <agatevureglory@gmail.com>
2020-03-08 09:22:20 +0100

    This handles UI configuration changes and communicates with the repository

---
+++ app/src/main/java/com/chuks/maizestemapp/capturedinsect/viewmodel/CapturedInsectViewModel.kt
@@ -0,0 +1,44 @@
+package com.chuks.maizestemapp.capturedinsect.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.chuks.maizestemapp.common.data.Insect
+import com.chuks.maizestemapp.capturedinsect.repository.CapturedInsectRepository
+import kotlinx.coroutines.launch
+
+/**
+ * This is CapturedInsectViewModel. This has a param [capturedInsectRepository]
+ * and extends [ViewModel]
+ */
+class CapturedInsectViewModel(private val capturedInsectRepository: CapturedInsectRepository)
+    : ViewModel() {
+
+    private val _showMessage : MutableLiveData<String> = MutableLiveData()
+    val showMessage : LiveData<String> get() = _showMessage
+
+    private val _showProgress : MutableLiveData<Boolean> = MutableLiveData()
+    val showProgress : LiveData<Boolean> get() = _showProgress
+
+
+    val capturedInsect = capturedInsectRepository.getAllCapturedInsect()
+
+    init {
+
+        viewModelScope.launch {
+            try {
+                _showProgress.postValue(true)
+                capturedInsectRepository.requestCapturedInsect()
+                _showProgress.postValue(false)
+
+            }catch (e : Throwable){
+                _showMessage.postValue("Unable to connect to server!")
+                _showProgress.postValue(false)
+            }
+        }
+    }
+
+
+
+}
