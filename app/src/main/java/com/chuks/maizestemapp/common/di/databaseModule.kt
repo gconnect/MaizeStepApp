202750d57f60b5815c4e8d3840efc7e18dcdcd28
gconnect <agatevureglory@gmail.com>
2020-03-08 09:44:29 +0100

    Database module is a single instance of database for dependency injection

---
+++ app/src/main/java/com/chuks/maizestemapp/common/di/databaseModule.kt
@@ -0,0 +1,63 @@
+package org.wikiedufoundation.wikiedudashboard.di
+
+import android.app.Application
+import androidx.room.Room.databaseBuilder
+import com.chuks.maizestemapp.capturedinsect.dao.CapturedInsectDao
+import com.chuks.maizestemapp.categoriesofspecies.africanarmyworm.dao.AfricanArmyWormDao
+import com.chuks.maizestemapp.categoriesofspecies.egyptianarmyworm.dao.EgyptianArmyWormDao
+import com.chuks.maizestemapp.categoriesofspecies.fallarmyworm.dao.FallArmyWormDao
+import com.chuks.maizestemapp.common.data.localdatabase.InsectDatabase
+import com.chuks.maizestemapp.maizestemplot.dao.MaizePlotDao
+import org.koin.android.ext.koin.androidApplication
+import org.koin.dsl.module
+
+
+/**
+ * Use the [databaseModule] to creating database and dao instance
+ **/
+val databaseModule = module {
+    single { provideDatabase(androidApplication()) }
+    single { provideCapturedInsectDao(get()) }
+    single { provideAfricanWormDao(get()) }
+    single { provideEgyptianWormDao(get()) }
+    single { provideFallArmyWormDao(get()) }
+    single { provideMaizePlotDao(get()) }
+}
+
+/**
+ * Use the [provideDatabase] to provide a database instance
+ * */
+fun provideDatabase(application: Application): InsectDatabase {
+    return databaseBuilder(application, InsectDatabase::class.java, "insect_database")
+            .fallbackToDestructiveMigration()
+            .allowMainThreadQueries()
+            .build()
+}
+
+/**
+ * Use the [provideCapturedInsectDao] to provide a provideCampaignListDao instance
+ * */
+fun provideCapturedInsectDao(database: InsectDatabase): CapturedInsectDao = database.capturedInsectDao
+
+
+/**
+* Use the [provideAfricanWormDao] to provide a provideCampaignListDao instance
+* */
+fun provideAfricanWormDao(database: InsectDatabase): AfricanArmyWormDao = database.africanArmyWormDao
+
+/**
+ * Use the [provideEgyptianWormDao] to provide a provideCampaignListDao instance
+ * */
+fun provideEgyptianWormDao(database: InsectDatabase): EgyptianArmyWormDao = database.egyptianArmyWormDao
+
+
+/**
+ * Use the [provideFallArmyWormDao] to provide a provideCampaignListDao instance
+ * */
+fun provideFallArmyWormDao(database: InsectDatabase): FallArmyWormDao = database.fallArmyWormDao
+
+/**
+ * Use the [provideFallArmyWormDao] to provide a provideCampaignListDao instance
+ * */
+fun provideMaizePlotDao(database: InsectDatabase): MaizePlotDao = database.maizePlot
+
