a6f90267c22b99d45478b35c6997a7b00d66d430
gconnect <agatevureglory@gmail.com>
2020-03-08 09:41:23 +0100

    Changed the file location and added comment

---
+++ app/src/main/java/com/chuks/maizestemapp/common/data/InsectResponse.kt
@@ -0,0 +1,12 @@
+package com.chuks.maizestemapp.common.data
+
+/**
+ * This is the [InsectResponse]  data class
+ * */
+data class InsectResponse(val insect:List<Insect>)
+
+
+/**
+ * This is the [InsectResponse]  data class
+ * */
+data class MaizePlotResponse(val insect_plot : ArrayList<MaizePlot>)
