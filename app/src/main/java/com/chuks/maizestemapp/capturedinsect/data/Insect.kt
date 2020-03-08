f5c47f2bca7943ff484cabcca6283418d1596c21
gconnect <agatevureglory@gmail.com>
2020-03-08 09:19:02 +0100

    Created Insect data class and added comments

--- app/src/main/java/com/chuks/maizestemapp/capturedinsect/data/Insect.kt
+++
@@ -1,8 +0,0 @@
-package com.chuks.maizestemapp.capturedinsect.data
-
-data class Insect(
-    val insectImage: String, val name: String, val location: String,
-    val date: String, val time: String, val count: Int
-)
-
-
